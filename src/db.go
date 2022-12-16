fbb19b472ba599c63031e32d5a1a96ca47594b00
astralaria <pawut.j@gmail.com>
2022-12-17 04:21:27 +0700

    Remove comment

--- src/db.go
+++ src/db.go
@@ -16,22 +16,5 @@ func ConnectDB() *sql.DB {
 		log.Fatal(err)
 	}
 
-	// createProductTable := `CREATE TABLE IF NOT EXISTS product (id SERIAL  PRIMARY KEY , name TEXT , price INTEGER);`
-	// _, err = db.Exec(createProductTable)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// row := db.QueryRow("Insert into product (name, price) values ($1 , $2) RETURNING ID", "SomeProduct", 1)
-	// var id int
-	// err = row.Scan(&id)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("insert complate", id)
-
 	return db
 }
